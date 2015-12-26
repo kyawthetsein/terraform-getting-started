96cf0ed58643ef96ea2bd80f4d7e2d2440541324
kyawthetsein <kyawthetsein@gmail.com>
2015-12-26 06:42:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = "us-east-1"
-}
-
 resource "aws_instance" "example" {
     ami = "ami-d05e75b8"
     instance_type = "t2.micro"
