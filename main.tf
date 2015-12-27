5bc7dcd50625e1e235f0c33f8913a4aa120c7281
kyawthetsein <kyawthetsein@gmail.com>
2015-12-27 04:43:47 +0000

    Added user and key_file

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
 resource "aws_instance" "example" {
+
+    connection {
+      user = "ubuntu"
+
+      key_file = "${var.key_path}"
+
+    }
+
     ami = "ami-ef5e24df"
+
     instance_type = "t2.micro"
+
 }
 
 resource "aws_eip" "ip" {
+
     instance = "${aws_instance.example.id}"
+
 }
