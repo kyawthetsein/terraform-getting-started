1516fcaa571e4a102f633884a941570e45dca983
kyawthetsein <kyawthetsein@gmail.com>
2016-01-08 06:06:31 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 
   variable "AWS_ACCESS_KEY_ID" {}
   variable "AWS_SECRET_KEY" {}
-  variable "key" {}
-  variable "ATLAS_TOKEN" {}
+  #variable "key" {}
+  #variable "ATLAS_TOKEN" {}
   variable "aws_region" {
       default = "us-west-2"
   }    
