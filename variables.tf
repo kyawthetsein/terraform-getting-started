4b35a735d617433bdc0dfeac44e99e7b0cab1e89
kyawthetsein <kyawthetsein@gmail.com>
2016-01-08 06:00:28 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
-{ 
-  "variables": {
-    "aws_access_key": "{{env `AWS_ACCESS_KEY_ID`}}"
-    "aws_secret_key": "{{env `AWS_SECRET_KEY`}}"
-    "aws_ssh_key": "{{env `key`}}"
-    "atlas_token": "{{env `ATLAS_TOKEN`}}"
-    variable "aws_region": "{
-    default = 'us-west-2'}"
-  }
-}
+
+  variable "AWS_ACCESS_KEY_ID" {}
+  variable "AWS_SECRET_KEY" {}
+  variable "key" {}
+  variable "ATLAS_TOKEN" {}
+  variable "aws_region" {
+      default = "us-west-2"
+  }    
+      
 
