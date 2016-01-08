d8b5568f9ad60a6642a2310abcf802f26221ab0c
kyawthetsein <kyawthetsein@gmail.com>
2016-01-08 05:01:45 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "key" {}
+variable "access_key" {{env `AWS_ACCESS_KEY_ID`}}
+variable "secret_key" {{env `AWS_SECRET_KEY`}}
+variable "key" {{env `key`}}
 variable "region" {
     default = "us-west-2"
 }
