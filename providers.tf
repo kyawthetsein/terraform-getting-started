1b61b907d9ba8d6d04ec1279490195edf1fb5167
kyawthetsein <kyawthetsein@gmail.com>
2016-01-08 05:18:33 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,4 +2,4 @@ provider "aws" {
     access_key = ENV['aws_access_key']
     secret_key = ENV['aws_secret_key']
     key = ENV['aws_ssh_key']
-    region = ENV['region']
+    region = ENV['aws_region']
